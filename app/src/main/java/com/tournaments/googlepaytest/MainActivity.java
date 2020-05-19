06d330d7d1789ea621c73d811089bdf8d77e30c5
HashimJVZ <hashimtvr2@gmail.com>
2020-05-19 17:54:26 +0530

    Initial commit

---
+++ app/src/main/java/com/tournaments/googlepaytest/MainActivity.java
@@ -0,0 +1,93 @@
+package com.tournaments.googlepaytest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tournaments.googlepaytest.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
+    int GOOGLE_PAY_REQUEST_CODE = 123;
+    String amount;
+    String name = "MRUDUL K M";
+    String upiId = "mr18yt@okaxis";
+    String transactionNote = "pay test";
+    String status;
+    Uri uri;
+    private ActivityMainBinding binding;
+
+    private static boolean isAppInstalled(Context context, String packageName) {
+        try {
+            context.getPackageManager().getApplicationInfo(packageName, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private static Uri getUpiPaymentUri(String name, String upiId, String transactionNote, String amount) {
+        return new Uri.Builder()
+                .scheme("upi")
+                .authority("pay")
+                .appendQueryParameter("pa", upiId)
+                .appendQueryParameter("pn", name)
+                .appendQueryParameter("tn", transactionNote)
+                .appendQueryParameter("am", amount)
+                .appendQueryParameter("cu", "INR")
+                .build();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.googlePayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amount = binding.amountEditText.getText().toString();
+                if (!amount.isEmpty()) {
+                    uri = getUpiPaymentUri(name, upiId, transactionNote, amount);
+                    payWithGPay();
+                } else {
+                    binding.amountEditText.setError("Amount is required!");
+                    binding.amountEditText.requestFocus();
+                }
+
+            }
+        });
+    }
+
+    private void payWithGPay() {
+        if (isAppInstalled(this, GOOGLE_PAY_PACKAGE_NAME)) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(uri);
+            intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
+            startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
+        } else {
+            Toast.makeText(MainActivity.this, "Please Install GPay", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            status = data.getStringExtra("Status").toLowerCase();
+        }
+
+        if ((RESULT_OK == resultCode) && status.equals("success")) {
+            Toast.makeText(MainActivity.this, "Transaction Successful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Transaction Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
